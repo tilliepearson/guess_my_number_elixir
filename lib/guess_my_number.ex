89705fd14ec15496565a4288547c65a8758d61b6
Tillie <tillie.pearson@wearesauce.io>
2021-05-13 14:14:02 +0100

    improved play function

--- lib/guess_my_number.ex
+++ lib/guess_my_number.ex
@@ -1,31 +1,29 @@
 defmodule GuessMyNumber do
   @moduledoc """
-   This is my very nice guess the number game
+    This is my very nice guess the number game
     call GuessMyNumber.new_game to play
   """
   alias GuessMyNumber.Game
 
+  @doc """
+
+  """
   def new_game do
    ask_for_number("Pick the max number for this game (1 - X)") |> Game.init() |> play()
   end
 
   @spec play(%Game{}) :: any
-  defp play(state) do
-    guess = ask_for_number("Enter a number")
-    state = Game.increment_guesses(state)
-
-    cond do
-      Game.check_guess(state, guess) == :high ->
-        IO.puts("That is too high :(")
-        play(state)
-
-      Game.check_guess(state, guess) == :low ->
-        IO.puts("That is too low :o")
-        play(state)
-
-      Game.check_guess(state, guess) == :ok ->
-        IO.puts("YOU GOT IT in #{Game.guesses(state)} guesses")
-    end
+defp play(state) do
+  {state, guess_response} = Game.guess(state, ask_for_number("Enter a number"))
+  case guess_response do
+    :high ->
+      IO.puts("That is too high :(")
+      play(state)
+    :low ->
+      IO.puts("That is too low :o")
+      play(state)
+    :ok ->
+      IO.puts("YOU GOT IT in #{Game.guesses(state)} guesses")
   end
 
   defp ask_for_number(question) do
