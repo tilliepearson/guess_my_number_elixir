380755ad85d5e52a05f00974adfb60d87d8ef7e5
Tillie <tillie.pearson@wearesauce.io>
2021-05-14 15:19:23 +0100

    improved logic

--- lib/game.ex
+++ lib/game.ex
@@ -5,14 +5,12 @@ defmodule GuessMyNumber.Game do
     %GuessMyNumber.Game{number: Enum.random(1..number), guesses: 0}
   end
 
-    def check_guess(state, guess) when guess > state.number, do: :high
-    def check_guess(state, guess) when guess < state.number, do: :low
-    def check_guess(_state, _guess), do: :ok
+  def check_guess(state, guess) when guess > state.number, do: :high
+  def check_guess(state, guess) when guess < state.number, do: :low
+  def check_guess(_state, _guess), do: :ok
 
-    
-
-  def increment_guesses(state) do
-    %{state | guesses: state.guesses + 1}
+  def guess(state, guess) do
+    {%{state | guesses: state.guesses + 1}, check_guess(state, guess)}
   end
 
   def guesses(state) do
--- lib/guess_my_number.ex
+++ lib/guess_my_number.ex
@@ -9,21 +9,25 @@ defmodule GuessMyNumber do
 
   """
   def new_game do
-   ask_for_number("Pick the max number for this game (1 - X)") |> Game.init() |> play()
+    ask_for_number("Pick the max number for this game") |> Game.init() |> play()
   end
 
   @spec play(%Game{}) :: any
-defp play(state) do
-  {state, guess_response} = Game.guess(state, ask_for_number("Enter a number"))
-  case guess_response do
-    :high ->
-      IO.puts("That is too high :(")
-      play(state)
-    :low ->
-      IO.puts("That is too low :o")
-      play(state)
-    :ok ->
-      IO.puts("YOU GOT IT in #{Game.guesses(state)} guesses")
+  defp play(state) do
+    {state, guess_response} = Game.guess(state, ask_for_number("Enter a number"))
+
+    case guess_response do
+      :high ->
+        IO.puts("That is too high :(")
+        play(state)
+
+      :low ->
+        IO.puts("That is too low :o")
+        play(state)
+
+      :ok ->
+        IO.puts("YOU GOT IT in #{Game.guesses(state)} guesses")
+    end
   end
 
   defp ask_for_number(question) do
